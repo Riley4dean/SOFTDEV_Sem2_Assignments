8eb3d9084c0cff7940db781713f352af011f9153
Riley Dean <deanril000@warren.k12.in.us>
2024-01-11 10:40:14 -0500

    firstassignment

--- 01_Semester 1 Project Review/Assignment/src/App.java
+++ 01_Semester 1 Project Review/Assignment/src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(Systen.in);
+        int firstNumber = input.nextInt();
+        int secondNumber = input.nextInt();
+
+        int quotient = 0;
+        try{
+            quotient = firstNumber / secondNumber;
+        } catch(ArithmeticException ex){
+            System.out.println(ex.getMessage());
+        } finally{
+            System.out.println(String.format("the answer from division is %s", quotient));
+        }
     }
 }
