95db2da0140bbf58edc010273931d5a052737e66
Riley Dean <deanril000@warren.k12.in.us>
2024-01-31 10:52:04 -0500

    dummy!

--- 03_File Handling/02_Reading a File/Assignment/src/App.java
+++ 03_File Handling/02_Reading a File/Assignment/src/App.java
@@ -1,5 +1,19 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String fileName = "src/Assignment.txt";
+
+        Path filePath = Paths.get(fileName);
+        List<String> lines = Files.readAllLines(filePath);
+        for (String i : lines){
+            System.out.println(i);
+        }
+        
     }
+
 }
+
