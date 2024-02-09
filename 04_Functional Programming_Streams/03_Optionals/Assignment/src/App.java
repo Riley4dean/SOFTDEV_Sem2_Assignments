56ae6e9155c0cd19f42eefc3fbac9f224af3dbe4
Riley Dean <deanril000@warren.k12.in.us>
2024-02-09 10:53:16 -0500

    woo

--- 04_Functional Programming_Streams/03_Optionals/Assignment/src/App.java
+++ 04_Functional Programming_Streams/03_Optionals/Assignment/src/App.java
@@ -1,5 +1,14 @@
+import java.util.function.Function;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {   
+        Function<Integer, Integer> subtract5 = (num) -> num - 5;
+        Function<Integer, Integer> Times5 = (num) -> num * 5;
+        Function<Integer, Integer> subtract5Times5 = subtract5.andThen(Times5);
+        int testNumber = 1;
+        var answer = subtract5Times5.apply(testNumber);
+        System.out.println(answer);
+
     }
+
 }
