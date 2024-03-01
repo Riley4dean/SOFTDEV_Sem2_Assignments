cd4eb699298b464a0af53bd98ac725a4b5f084e7
Riley Dean <deanril000@warren.k12.in.us>
2024-03-01 10:32:25 -0500

    sparkle release

--- Unit_06_Parallel Programming/02_Completable Future/Assignment/src/App.java
+++ Unit_06_Parallel Programming/02_Completable Future/Assignment/src/App.java
@@ -1,5 +1,15 @@
+import java.util.concurrent.CompletableFuture;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        CompletableFuture<Double> future = CompletableFuture.supplyAsync(
+            ()-> Math.random()
+        );
+
+        CompletableFuture<Void> futureRandom = future.thenAccept(s->{
+            System.out.println(s * 2);
+        });
+
+        futureRandom.get();
     }
 }
