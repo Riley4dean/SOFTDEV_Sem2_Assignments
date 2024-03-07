44890d90a89d031cf06d8f542177bd5e6c20cbac
Riley Dean <deanril000@warren.k12.in.us>
2024-03-07 10:58:14 -0500

    pull

--- Unit_06_Parallel Programming/Intro/Assignment/src/App.java
+++ Unit_06_Parallel Programming/Intro/Assignment/src/App.java
@@ -1,19 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-<<<<<<< HEAD
+
         for(int i = 0; i <= 10000; i++){
             SqrtRunnable sqrt = new SqrtRunnable(i);
             sqrt.start();
         }
-=======
-        
-        for(int i = 0; i <= 10000; i++){
-            //TODO: //Consruct a SqrtRunnable passing i
-            //TODO: //start the SqrtRunnable with the start(method)
-        }
 
 
-
->>>>>>> 1de715d5244a9cf685c2d47bcb6bb19f56ed0fb4
     }
 }
